ba79cdcf74117032e60fa42cb92b0ce2f6a805e9
Vinicius <vinciuscarmo95@gmail.com>
2024-05-07 01:02:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ import express from "express";
 import userRoutes from "./routes/user.js";
 import cors from "cors";
 
-const port = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use("/", userRoutes);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.listen(3000);
