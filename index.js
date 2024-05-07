b1cfaed81eac83fcdf5ace47657efacea3bcde82
Vinicius <vinciuscarmo95@gmail.com>
2024-05-07 01:10:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/", userRoutes);
+app.use("/users", userRoutes);
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-  console.log("Server Started");
-});
+app.listen(3000);
