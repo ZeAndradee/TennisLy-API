f92521eab14a040fc1d4a1f10bd61eafdf82e1f5
Vinicius <vinciuscarmo95@gmail.com>
2024-05-10 20:17:43 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,18 @@ import url from "url";
 
 const dbUrl = url.parse(process.env.DATABASE_URL);
 const [user, password] = dbUrl.auth.split(":");
-
+console.log("Host:", dbUrl.hostname);
+console.log("Port:", dbUrl.port);
+console.log("User:", user);
+console.log("Password:", password);
+console.log("Database:", dbUrl.pathname.substr(1));
 const connection = mysql.createConnection({
   host: dbUrl.hostname,
   port: dbUrl.port,
   user: user,
   password: password,
-  database: dbUrl.pathname.substr(1), // remove the leading '/'
+  database: dbUrl.pathname.substr(1),
+  // remove the leading '/'
 });
 
 export default connection;
