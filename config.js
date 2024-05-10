2ae0480c8ac1f706237429a8dc19c3b3edd54db7
Vinicius <vinciuscarmo95@gmail.com>
2024-05-10 20:00:47 -0300

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ dotenvConfig();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
