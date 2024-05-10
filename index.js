e1e51291f6d9cb5edf9600eb6d06727a8fa9e4c1
Vinicius <vinciuscarmo95@gmail.com>
2024-05-10 00:20:18 -0300

    update files

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-import express from "express";
-import userRoutes from "./routes/user.js";
-import cors from "cors";
-const app = express();
+const express = require("express")
+const app = express()
 
-app.use(express.json());
-app.use(cors());
-app.use("/users", userRoutes);
+require('dotenv').config()
 
-app.listen(3000);
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+const postsRouter = require('./routes/posts.router')
+const authRouter = require('./routes/auth.router')
+
+app.use("/api/v1/posts", postsRouter)
+app.use("/api/v1/auth", authRouter)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log("Server is running....")
+})
