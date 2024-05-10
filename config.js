7783deba7ae0a4afb20f3dadf633c76ec3c745e3
Vinicius <vinciuscarmo95@gmail.com>
2024-05-10 20:48:33 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,12 @@
+import { config as dotenvConfig } from "dotenv";
 import mysql from "mysql2";
-import url from "url";
 
-const dbUrl = url.parse(process.env.DATABASE_URL);
-const [user, password] = dbUrl.auth.split(":");
-console.log("Host:", dbUrl.hostname);
-console.log("Port:", dbUrl.port);
-console.log("User:", user);
-console.log("Password:", password);
-console.log("Database:", dbUrl.pathname.substr(1));
 const connection = mysql.createConnection({
-  host: dbUrl.hostname,
-  port: dbUrl.port,
-  user: user,
-  password: password,
-  database: dbUrl.pathname.substr(1),
-  // remove the leading '/'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 export default connection;
