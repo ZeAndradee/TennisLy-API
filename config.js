a3f5fa307282333bcebdccadf9e813f75727a7ca
Vinicius <vinciuscarmo95@gmail.com>
2024-05-10 19:12:42 -0300

    zero again

--- config.js
+++ config.js
@@ -1,9 +1,13 @@
-import mysql from "mysql2";
 import { config as dotenvConfig } from "dotenv";
+import mysql from "mysql2";
 
 dotenvConfig();
 
-const urlDB = `mysql://root:GMkKaQnUZLFsnPjKMmUGmHMxIRGsOjoF@viaduct.proxy.rlwy.net:32479/railway`;
-const connection = mysql.createConnection(urlDB);
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
 
 export default connection;
