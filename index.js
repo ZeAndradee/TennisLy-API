8118bc337c234ea0e5debccef6c2b4a358c8d93c
Vinicius <vinciuscarmo95@gmail.com>
2024-05-10 17:07:32 -0300

    Locally Working

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-const express = require("express")
-const app = express()
+import express from "express";
+import userRoutes from "./routes/user.js";
+import cors from "cors";
 
-require('dotenv').config()
+const app = express();
 
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
-const postsRouter = require('./routes/posts.router')
-const authRouter = require('./routes/auth.router')
+app.use("/", userRoutes);
 
-app.use("/api/v1/posts", postsRouter)
-app.use("/api/v1/auth", authRouter)
-
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
-    console.log("Server is running....")
-})
+app.listen(3000);
