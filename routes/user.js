7ca22150a2d1e5649d42188b40eae44343a53006
Vinicius <vinciuscarmo95@gmail.com>
2024-05-12 16:48:24 -0300

    ff

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,8 @@
 import express from "express";
 import { getUsers } from "../controllers/user.js";
-import { getDBConnection } from "../config.js";
 
-getDBConnection();
 const router = express.Router();
+
 router.get("/", getUsers);
 
 export default router;
