53dcdb87bd376662da2624d4cb4dde50055b87f1
Vinicius <vinciuscarmo95@gmail.com>
2024-05-12 16:58:48 -0300

    update

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,11 @@
-import db from "../config.js";
+import { getDBConnection } from "../config.js";
 
 export const getUsers = (_, res) => {
   const q = "SELECT * FROM users";
+  const db = getDBConnection();
 
   db.query(q, (err, data) => {
+    db.end();
     if (err) return res.json(err);
 
     return res.status(200).json(data);
