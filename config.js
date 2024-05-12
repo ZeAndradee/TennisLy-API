6ac898314f8d66d6a08648e8f8b7b5197b2ef55c
Vinicius <vinciuscarmo95@gmail.com>
2024-05-12 18:03:01 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,15 @@
 import mysql from "mysql2";
 
 export function getDBConnection() {
-  return mysql.createConnection({
-    host: "viaduct.proxy.rlwy.net",
-    port: "32479",
-    user: "root",
-    password: "GMkKaQnUZLFsnPjKMmUGmHMxIRGsOjoF",
-    database: "railway",
-  });
+  try {
+    return mysql.createConnection({
+      host: "viaduct.proxy.rlwy.net",
+      port: "32479",
+      user: "root",
+      password: "GMkKaQnUZLFsnPjKMmUGmHMxIRGsOjoF",
+      database: "railway",
+    });
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+  }
 }
