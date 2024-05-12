89847215718e50a6f846c9525824af41a83987c2
Vinicius <vinciuscarmo95@gmail.com>
2024-05-12 17:53:12 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,11 @@
 import mysql from "mysql2";
 
 export function getDBConnection() {
-  console.log("DB_HOST:", process.env.DB_HOST);
-  console.log("DB_PORT:", process.env.DB_PORT);
-  console.log("DB_USER:", process.env.DB_USER);
-  console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-  console.log("DB_DATABASE:", process.env.DB_DATABASE);
   return mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: "viaduct.proxy.rlwy.net",
+    port: "32479",
+    user: "root",
+    password: "GMkKaQnUZLFsnPjKMmUGmHMxIRGsOjoF",
+    database: "railway",
   });
 }
