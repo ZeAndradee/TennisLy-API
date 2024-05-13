a6dab78ffcac46a02cde332a04ba17946947482c
Vinicius <vinciuscarmo95@gmail.com>
2024-05-13 19:28:06 -0300

    changes config.js

--- config.js
+++ config.js
@@ -3,11 +3,6 @@ import mysql from "mysql2";
 
 dotenvConfig();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 export default connection;
--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,9 @@
-import db from "../config.js";
+import connection from "../config.js";
 
 export const getUsers = (_, res) => {
   const q = "SELECT * FROM user";
 
-  db.query(q, (err, data) => {
+  connection.query(q, (err, data) => {
     if (err) return res.json(err);
 
     return res.status(200).json(data);
