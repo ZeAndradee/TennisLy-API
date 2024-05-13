15586700c077c8994f8b3ee0c6ba53af0b8108b2
Vinicius <vinciuscarmo95@gmail.com>
2024-05-13 19:51:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 import express from "express";
 import userRoutes from "./routes/user.js";
 import cors from "cors";
-import connection from "./config.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.get("/", (req, res) => {
-  connection.query(
-    "SELECT * FROM minha_tabela",
-    function (error, results, fields) {
-      if (error) {
-        console.error("Erro ao fazer a consulta: " + error.stack);
-        res.status(500).send("Erro ao fazer a consulta");
-        return;
-      }
 
-      res.send(
-        "Hello World! Aqui estão os resultados da sua consulta: " +
-          JSON.stringify(results)
-      );
-    }
-  );
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 // app.use("/", userRoutes);
 
