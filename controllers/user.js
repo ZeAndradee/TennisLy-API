4018ece72e756f34ee4912a30016f2daef079af4
Vinicius <vinciuscarmo95@gmail.com>
2024-05-18 19:15:54 -0300

    update implemented

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,5 @@
 import connection from "../config.js";
 
-export const getUsers = (_, res) => {
-  const q = "SELECT * FROM users";
-
-  connection.query(q, (err, data) => {
-    if (err) return res.json(err);
-
-    return res.status(200).json(data);
-  });
-};
-
 export const addUsers = (req, res) => {
   const q =
     "INSERT INTO users(`username`, `email`, `password`, `name`, `userimage`, `userbio`) VALUES(?)";
@@ -27,6 +17,35 @@ export const addUsers = (req, res) => {
     if (err) return res.json(err);
 
     return res.status(200).json("Usuario adicionado com sucesso.");
+  });
+};
+
+export const getUsers = (_, res) => {
+  const q = "SELECT * FROM users";
+
+  connection.query(q, (err, data) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json(data);
+  });
+};
+
+export const updateUser = (req, res) => {
+  const q =
+    "UPDATE users SET `id` = ?, `username` = ?, `email` = ?, `password` = ?, `name` = ?, `userimage` = ?, `userbio` = ?  WHERE `id` = ?";
+
+  const values = [
+    req.body.username,
+    req.body.email,
+    req.body.password,
+    req.body.name,
+    req.body.userimage,
+    req.body.userbio,
+  ];
+
+  connection.query(q, [...values], (err) => {
+    if (err) return res.json(err);
+    return res.status(200).json("Usuario alterado com sucesso.");
   });
 };
 
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { getUsers, addUsers, deleteUser } from "../controllers/user.js";
+import {
+  getUsers,
+  addUsers,
+  deleteUser,
+  updateUser,
+} from "../controllers/user.js";
 
 const router = express.Router();
 
+router.post("/users", addUsers);
+
 router.get("/users", getUsers);
 
-router.post("/users", addUsers);
+router.put("/users/:id", updateUser);
 
 router.delete("/users/:id", deleteUser);
 
