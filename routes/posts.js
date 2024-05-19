d882e8da5f511bd58712e887b65fcc319c125a7e
Vinicius <vinciuscarmo95@gmail.com>
2024-05-19 17:19:33 -0300

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { getPosts } from "../controllers/post.js";
+import { addPosts, getPosts } from "../controllers/post.js";
 
 const router = express.Router();
 
 router.get("/posts", getPosts);
 
-router.post("/posts", getPosts);
+router.post("/posts", addPosts);
 
 export default router;
