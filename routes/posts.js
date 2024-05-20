d7c62ec8ed717b21aca90d6d65e7f054057d574c
Vinicius <vinciuscarmo95@gmail.com>
2024-05-20 19:27:08 -0300

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addPosts, getPosts } from "../controllers/post.js";
+import { addPosts, deletePost, getPosts } from "../controllers/post.js";
 
 const router = express.Router();
 
@@ -7,6 +7,6 @@ router.get("/posts", getPosts);
 
 router.post("/posts", addPosts);
 
-router.delete("/posts/:postid");
+router.delete("/posts/:postid", deletePost);
 
 export default router;
