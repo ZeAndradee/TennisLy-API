2a7f5944f95dde17276f76fd19c78f0cd4ced572
Vinicius <vinciuscarmo95@gmail.com>
2024-05-31 18:46:02 -0300

    add delete update add

--- controllers/matches.js
+++ controllers/matches.js
@@ -1,24 +1,34 @@
 import connection from "../config.js";
 
-// export const addMatches = (req, res) => {
-//   const q =
-//     "INSERT INTO matches(`username`, `email`, `password`, `name`, `userimage`, `userbio`) VALUES(?)";
+export const addMatches = (req, res) => {
+  const q =
+    "INSERT INTO matches(`idplayer1`, `idplayer2`, `sets`, `matchtime`, `fscorep1`, `fscorep2`, `set1p1`, `set1p2`, `set2p1`, `set2p2`, `set3p1`, `set3p2`, `set4p1`, `set4p2`, `set5p1`, `set5p2`) VALUES(?)";
 
-//   const values = [
-//     req.body.username,
-//     req.body.email,
-//     req.body.password,
-//     req.body.name,
-//     req.body.userimage,
-//     req.body.userbio,
-//   ];
+  const values = [
+    req.body.idplayer1,
+    req.body.idplayer2,
+    req.body.sets,
+    req.body.matchtime,
+    req.body.fscorep1,
+    req.body.fscorep2,
+    req.body.set1p1,
+    req.body.set1p2,
+    req.body.set2p1,
+    req.body.set2p2,
+    req.body.set3p1,
+    req.body.set3p2,
+    req.body.set4p1,
+    req.body.set4p2,
+    req.body.set5p1,
+    req.body.set5p2,
+  ];
 
-//   connection.query(q, [values], (err) => {
-//     if (err) return res.json(err);
+  connection.query(q, [values], (err) => {
+    if (err) return res.json(err);
 
-//     return res.status(200).json("Usuario adicionado com sucesso.");
-//   });
-// };
+    return res.status(200).json("Partida adicionada com sucesso.");
+  });
+};
 
 export const getMatches = (_, res) => {
   const q = "SELECT * FROM matches";
@@ -30,32 +40,42 @@ export const getMatches = (_, res) => {
   });
 };
 
-// export const updateMatches = (req, res) => {
-//   const q =
-//     "UPDATE matches SET `username` = ?, `email` = ?, `password` = ?, `name` = ?, `userimage` = ?, `userbio` = ?  WHERE `id` = ?";
+export const updateMatches = (req, res) => {
+  const q =
+    "UPDATE matches SET `idplayer1`= ?, `idplayer2`= ?, `sets`= ?, `matchtime`= ?, `fscorep1`= ?, `fscorep2`= ?, `set1p1`= ?, `set1p2`= ?, `set2p1`= ?, `set2p2`= ?, `set3p1`= ?, `set3p2`= ?, `set4p1`= ?, `set4p2`= ?, `set5p1`= ?, `set5p2`= ?  WHERE `idmatch` = ?";
 
-//   const values = [
-//     req.body.username,
-//     req.body.email,
-//     req.body.password,
-//     req.body.name,
-//     req.body.userimage,
-//     req.body.userbio,
-//     req.params.id,
-//   ];
+  const values = [
+    req.body.idplayer1,
+    req.body.idplayer2,
+    req.body.sets,
+    req.body.matchtime,
+    req.body.fscorep1,
+    req.body.fscorep2,
+    req.body.set1p1,
+    req.body.set1p2,
+    req.body.set2p1,
+    req.body.set2p2,
+    req.body.set3p1,
+    req.body.set3p2,
+    req.body.set4p1,
+    req.body.set4p2,
+    req.body.set5p1,
+    req.body.set5p2,
+    req.params.idmatch,
+  ];
 
-//   connection.query(q, [...values], (err) => {
-//     if (err) return res.json(err);
-//     return res.status(200).json("Usuario alterado com sucesso.");
-//   });
-// };
+  connection.query(q, [...values], (err) => {
+    if (err) return res.json(err);
+    return res.status(200).json("Partida alterada com sucesso.");
+  });
+};
 
-// export const deleteMatches = (req, res) => {
-//   const q = "DELETE FROM matches WHERE `id` = ?";
+export const deleteMatches = (req, res) => {
+  const q = "DELETE FROM matches WHERE `idmatch` = ?";
 
-//   connection.query(q, [req.params.id], (err) => {
-//     if (err) return res.json(err);
+  connection.query(q, [req.params.idmatch], (err) => {
+    if (err) return res.json(err);
 
-//     return res.status(200).json("Match deletada com sucesso.");
-//   });
-// };
+    return res.status(200).json("Partida deletada com sucesso.");
+  });
+};
--- routes/matches.js
+++ routes/matches.js
@@ -1,15 +1,20 @@
 import express from "express";
 
-import { getMatches } from "../controllers/matches.js";
+import {
+  addMatches,
+  deleteMatches,
+  getMatches,
+  updateMatches,
+} from "../controllers/matches.js";
 
 const router = express.Router();
 
-// router.post("/matches", addMatches);
+router.post("/matches", addMatches);
 
 router.get("/matches", getMatches);
 
-// router.put("/matches/:id", updateMatches);
+router.put("/matches/:idmatch", updateMatches);
 
-// router.delete("/matches/:id", deleteMatches);
+router.delete("/matches/:idmatch", deleteMatches);
 
 export default router;
