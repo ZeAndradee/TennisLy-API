d5d6322466d3c718945e1064c12de09659db0e69
Vinicius <vinciuscarmo95@gmail.com>
2024-05-31 17:51:59 -0300

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { getPosts, addPosts, deletePost, upload } from "../controllers/post.js"; // Atualize o caminho conforme necessário
+import {
+  getPosts,
+  addPosts,
+  deletePost,
+  upload,
+  updatePost,
+} from "../controllers/post.js"; // Atualize o caminho conforme necessário
 
 const router = express.Router();
 
 router.get("/posts", getPosts);
 router.post("/posts", upload.single("image"), addPosts); // Usar o middleware de upload do Multer
 router.delete("/posts/:postid", deletePost);
-
+router.put("/posts/:postid", updatePost);
 export default router;
